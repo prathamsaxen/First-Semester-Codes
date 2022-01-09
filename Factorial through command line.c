028ffb355d8a57245a71cbc0811ff9b4bca37c59
Pratham <er.prathamsaxena@gmail.com>
2022-01-10 01:00:20 +0530

    Factorial through command line Added

---
+++ Factorial through command line.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<conio.h>
+#include<stdlib.h>
+int factorial(int a)
+{
+    int fac=1;
+    if(a==1)
+    {
+        return fac;
+    }
+    else 
+    {
+        fac=a*factorial(a-1);
+    }
+    return fac;
+}
+int main(int argc, char const *argv[])
+{
+    printf("Factorial [%d]=%d",atoi(argv[1]),factorial(atoi(argv[1])));
+    printf("\nThanks for using our program.\nDeveloped by Er Pratham Saxena");
+    getch();
+    return 0;
+}
