fdb5caf0284b5006a5a64784418abb4047fe82fd
Pratham <er.prathamsaxena@gmail.com>
2022-01-09 15:39:40 +0530

    Sum of Factorial Series Added

---
+++ Sum of Factorial Series.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <conio.h>
+int factorial_sum(int a)
+{
+    int factorial = 1;
+    if (a == 1)
+    {
+        return factorial;
+    }
+    else
+    {
+        for (int index = 1; index < a; index++)
+        {
+            factorial=a*factorial_sum(a-1);
+        }
+    }
+    return factorial;
+}
+void main()
+{
+    start:
+    printf("Welcome to program:-");
+    int a,sum=0,temp;
+    printf("\nHow Much Terms Of Series You Wanna Add: ");
+    scanf("%d",&a);
+    temp=a;
+    while(a>0)
+    {
+        sum=sum+factorial_sum(a);
+        a=a-1;
+    }
+    printf("The Sum of %d terms of your series is %d",a,sum);
+    char permit;
+    printf("\nIf You Wanna Use This Program Again.Press 'Y': ");
+    getchar();
+    scanf("%c", &permit);
+    if (permit = 'Y' && permit == 'y')
+    {
+        printf("Redirecting To Program \n");
+        goto start;
+    }
+    else
+    {
+        printf("Thanks for using our program.\nDeveloped by Er Pratham Saxena");
+    }
+    getch();
+}
