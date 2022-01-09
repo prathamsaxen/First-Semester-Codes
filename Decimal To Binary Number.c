b276d7da806076b7b235e06711bee2d3e0fcef07
Pratham <er.prathamsaxena@gmail.com>
2022-01-10 01:02:24 +0530

    Decimal To Binary Number Added

---
+++ Decimal To Binary Number.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <conio.h>
+int main()
+{
+start:
+    printf("Program to Convert Decimal Number into Binary\n");
+    int arr[10], num, i, j;
+    printf("Please Give a Number to Convert in Binary:  ");
+    scanf("%d", &num);
+    printf("Binary Number of %d is =  ", num);
+    for (i = 0; num > 0; i++)
+    {
+        arr[i] = num % 2;
+        num = num / 2;
+    }
+    for (j = i - 1; j >= 0; j--)
+    {
+        printf(" %d ", arr[j]);
+    }
+    char permit;
+    printf("\nIf You Wanna Use This Program Again.Press 'Y': ");
+    getchar();
+    scanf("%c", &permit);
+    if (permit = 'Y' && permit == 'y')
+    {
+        printf("Redirecting To Program \n");
+        goto start;
+    }
+    else
+    {
+        printf("Thanks for using our program.\nDeveloped by Er Pratham Saxena");
+    }
+    return 0;
+    getch();
+}
