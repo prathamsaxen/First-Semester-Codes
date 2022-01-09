99eb80d2d56597043e61055809dc24ee4d8ca848
Pratham <er.prathamsaxena@gmail.com>
2022-01-10 01:08:59 +0530

    5 Digit Sum Through Command Line Added

---
+++ 5 Digit Sum Through Command Line.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <conio.h>
+#include <stdlib.h>
+int main(int argc, char const *argv[])
+{
+    int sum = 0, remainder, num = atoi(argv[1]);
+    while (num > 0)
+    {
+        remainder = num % 10;
+        sum = sum + remainder;
+        num = num / 10;
+    }
+    printf("Sum[%d]=%d", atoi(argv[1]), sum);
+    printf("\nThanks for using our program.\nDeveloped by Er Pratham Saxena");
+    getch();
+    return 0;
+}
