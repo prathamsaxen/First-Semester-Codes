7e8a24e3948dfb6c2233f63b4dd0957ee0d1bcf6
Pratham <er.prathamsaxena@gmail.com>
2022-01-10 01:06:22 +0530

    Area Of Rectangle Added

---
+++ Area Of Rectangle.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+float react_area(float a,float b)
+{
+    return a*b;
+}
+void main()
+{
+    printf("Area Of Rectangle:-\n");
+    float length,breadth;
+    printf("Enter The Length of Reactangle: ");
+    scanf("%f",&length);
+    printf("Enter The Breadth Of Rectangle: ");
+    scanf("%f",&breadth);
+    printf("Area :- %f",react_area(length,breadth));
+    printf("\nDeveloped by Er Pratham Saxena");
+    getch();
+    }
