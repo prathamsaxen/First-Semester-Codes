a59af502fda549c428a421c80ded7666b18bfe02
Pratham <er.prathamsaxena@gmail.com>
2022-01-10 00:53:19 +0530

    Numbers Printing Added

---
+++ Numbers Printing.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <conio.h>
+int main()
+{
+start:
+    printf("Welcome To Our Program:-\n");
+    for (int index = 3; index > 0; index--)
+    {
+        for (int index2 = index; index2 > 0; index2--)
+        {
+            printf("%d", index2);
+        }
+        printf("\n");
+    }
+    printf("Thanks for using our program.\nDeveloped by Er Pratham Saxena");
+    return 0;
+    getch();
+}
