5fcb68b1381ae378ca3bcdb335734f2e09a3a9bb
Pratham <er.prathamsaxena@gmail.com>
2022-01-10 00:51:30 +0530

    Remove Blank Spaces From String ADDED

---
+++ Remove Blank Spaces From String.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <conio.h>
+#include <string.h>
+void main()
+{
+start:
+    printf("Welcome To Our Program");
+    char text[100], blank[100];
+    int c = 0, d = 0;
+
+    printf("\nEnter some text: ");
+
+    gets(text);
+    while (text[c] != '\0')
+    {
+        if (text[c] != ' ')
+        {
+            blank[d] = text[c];
+            d++;
+        }
+        c++;
+    }
+    blank[d] = '\0';
+    printf("Text after removing blanks\n%s\nNumber of Characters=%d\n", blank,strlen(blank));
+
+    printf("Thanks for using our program.\nDeveloped by Er Pratham Saxena");
+    getch();
+}
