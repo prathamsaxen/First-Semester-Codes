84df13b2b086bc71e1aea8de6ebadd0404e2bfae
Pratham <er.prathamsaxena@gmail.com>
2022-01-10 00:59:10 +0530

    Five_Digit_to_Single_Line_Print Added

---
+++ Five_Digit_to_Single_Line_Print.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    printf("Welcome To Our Program:-\n");
+    int a,b,c,d,e;
+    printf("Enter 5 Digit Number Vetically:-\n");
+    scanf("%d%d%d%d%d",&a,&b,&c,&d,&e);
+    printf("%d",a);
+    printf("%d",b);
+    printf("%d",c);
+    printf("%d",d);
+    printf("%d",e);
+    printf("\nDeveloped by Er Pratham Saxena");
+    getch();
+}
