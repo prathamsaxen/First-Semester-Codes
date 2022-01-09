750849f58c3e8720426d829f8a447fb3c2df2dad
Pratham <er.prathamsaxena@gmail.com>
2022-01-10 01:03:07 +0530

    Check String is Palindrome Added

---
+++ Check String is Palindrome.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <conio.h>
+#include <string.h>
+
+int main()
+{
+    start:
+    printf("Welcome to our program\n");
+    char s1[1000], s2[1000];
+    printf("Enter  the string: ");
+    scanf("%s", s1);
+    strcpy(s2, s1);
+    strrev(s2);
+    if (!strcmp(s1, s2))
+        printf("string is palindrome");
+    else
+        printf("string is not palindrome");
+    char permit;
+    printf("\nIf You Wanna Use This Program Again.Press 'Y': ");
+    getchar();
+    scanf("%c", &permit);
+    if (permit = 'Y' && permit == 'y')
+    {
+        printf("Redirecting To Program \n");
+        goto start;
+    }
+    else
+    {
+        printf("Thanks for using our program.\nDeveloped by Er Pratham Saxena");
+    }
+
+    getch();
+    return 0;
+}
