a57dc237eb40a1561157c3d451b334d353be7e01
Madhvendra Singh <86185962+madhvendra-singh@users.noreply.github.com>
2022-03-07 19:10:43 +0530

    Update Sum of Factorial Series.c

--- Sum of Factorial Series.c
+++ Sum of Factorial Series.c
@@ -41,7 +41,7 @@ void main()
     }
     else
     {
-        printf("Thanks for using our program.\nDeveloped by Er Pratham Saxena");
+        printf("Thanks for using our program.\nDeveloped by Madhvendra Singh");
     }
     getch();
 }
