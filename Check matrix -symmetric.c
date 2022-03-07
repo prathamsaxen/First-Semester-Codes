5429ea6ce4a97917038ec736f1874818e9da612e
Madhvendra Singh <86185962+madhvendra-singh@users.noreply.github.com>
2022-03-07 19:05:21 +0530

    Update Check matrix -symmetric.c

--- Check matrix -symmetric.c
+++ Check matrix -symmetric.c
@@ -69,7 +69,7 @@ start:
     }
     else
     {
-        printf("Thanks for using our program.\nDeveloped by Er Pratham Saxena");
+        printf("Thanks for using our program.\nDeveloped by Madhvendra Singh");
     }
     getch();
 }
