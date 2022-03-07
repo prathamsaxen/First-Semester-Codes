8eca6f666e4fd663c07ed9b1d92131010ece3312
Madhvendra Singh <86185962+madhvendra-singh@users.noreply.github.com>
2022-03-07 19:07:56 +0530

    Update GCD of Two Given Numbers.c

--- GCD of Two Given Numbers.c
+++ GCD of Two Given Numbers.c
@@ -29,7 +29,7 @@ char permit;
     }
     else
     {
-        printf("Thanks for using our program.\nDeveloped by Er Pratham Saxena");
+        printf("Thanks for using our program.\nDeveloped by Madhvendra Singh");
     }
     getch();
 }
