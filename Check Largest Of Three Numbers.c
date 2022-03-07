397501d3e28bd3c10fc2cdc006c8fe46575b3a41
Madhvendra Singh <86185962+madhvendra-singh@users.noreply.github.com>
2022-03-07 19:04:49 +0530

    Update Check Largest Of Three Numbers.c

--- Check Largest Of Three Numbers.c
+++ Check Largest Of Three Numbers.c
@@ -52,7 +52,7 @@ start:
     }
     else
     {
-        printf("Thanks for using our program.\nDeveloped by Er Pratham Saxena");
+        printf("Thanks for using our program.\nDeveloped by Madhvendra Singh");
     }
     getch();
 }
