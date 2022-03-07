e525cfcd9277b78a2993e4c45bfb0ed0252244f4
Madhvendra Singh <86185962+madhvendra-singh@users.noreply.github.com>
2022-03-07 19:03:05 +0530

    Update Addition Of Diagonal of Square Matrix.c

--- Addition Of Diagonal of Square Matrix.c
+++ Addition Of Diagonal of Square Matrix.c
@@ -49,7 +49,7 @@ start:
     }
     else
     {
-        printf("Thanks for using our program.\nDeveloped by Er Pratham Saxena");
+        printf("Thanks for using our program.\nDeveloped by Madhvendra Singh");
     }
     getch();
 }
