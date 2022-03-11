339a13a795695b524f8044b00fb5608af67c8050
Madhvendra Singh <86185962+madhvendra-singh@users.noreply.github.com>
2022-03-11 20:29:03 +0530

    Update Factorial through command line.c

--- Factorial through command line.c
+++ Factorial through command line.c
@@ -17,7 +17,7 @@ int factorial(int a)
 int main(int argc, char const *argv[])
 {
     printf("Factorial [%d]=%d",atoi(argv[1]),factorial(atoi(argv[1])));
-    printf("\nThanks for using our program.\nDeveloped by Madhvendra Singh");
+    printf("\nThanks for using our program.");
     getch();
     return 0;
 }
